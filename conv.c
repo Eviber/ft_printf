a46ac38a1720905b90966695a017ece2111ba733
Youva GAUDE <ygaude@student.42.fr>
2017-07-31 06:13:30 +0200

    ullitoa() and llitoa()

--- conv.c
+++ conv.c
@@ -6,7 +6,7 @@
 /*   By: ygaude <ygaude@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/28 14:38:39 by ygaude            #+#    #+#             */
-/*   Updated: 2017/07/28 19:04:35 by ygaude           ###   ########.fr       */
+/*   Updated: 2017/07/31 05:40:08 by ygaude           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,15 @@ char	*addchar(char c, char **str)
 	return (buf);
 }
 
-char	*ullitoa(unsigned long long int n)
+char	ntoc(unsigned int n)
 {
-	int		i;
-	char	c;
+	return((n < 10) ? n + '0' : n - 10 + 'a');
+}
+
+char	*ullitoa(unsigned long long int n, unsigned int base)
+{
 	char	*str;
 
-	i = 0;
 	str = NULL;
 	if (!n)
 		str = addchar('0', &str);
@@ -36,9 +38,19 @@ char	*ullitoa(unsigned long long int n)
 	{
 		while (n)
 		{
-			str = addchar(n % 10 + '0', &str);
-			n /= 10;
+			str = addchar(ntoc(n % base), &str);
+			n /= base;
 		}
 	}
 	return (str);
+}
+
+char	*llitoa(long long int n, unsigned int base)
+{
+	char	*str;
+	char	*tmp;
+
+	tmp = "-";
+	str = ullitoa((unsigned long long)((n < 0) ? -n : n), base);
+	return((n < 0) ? ft_strappend(&tmp, &str, 'S') : str);
 }
--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-char	*ullitoa(unsigned long long int n);
+char	*ullitoa(unsigned long long int n, unsigned int base);
+char	*llitoa(long long int n, unsigned int base);
 
 int		main(int argc, char **argv)
 {
-	unsigned long long int n = 18446744073709551615;
-
-	printf("%s\n", ullitoa(n));
+	if (argc == 3)
+		printf("%s\n", llitoa(atoi(argv[1]), atoi(argv[2])));
+	else
+		printf("usage: ./a.out value base\n");
 	return (0);
 }
