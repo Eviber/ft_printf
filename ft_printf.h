b0faeae3c66de65883fb7fbdfde12930e84f2dd6
Youva GAUDE <ygaude@student.42.fr>
2017-08-20 14:16:48 +0200

    add header protection

--- ft_printf.h
+++ ft_printf.h
@@ -6,15 +6,18 @@
 /*   By: ygaude <ygaude@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/17 02:09:51 by ygaude            #+#    #+#             */
-/*   Updated: 2017/08/20 13:15:02 by ygaude           ###   ########.fr       */
+/*   Updated: 2017/08/20 14:08:54 by ygaude           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define SHARP 1
-#define MINUS 2
-#define PLUS 4
-#define SPACE 8
-#define ZERO 16
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# define SHARP 1
+# define MINUS 2
+# define PLUS 4
+# define SPACE 8
+# define ZERO 16
 
 typedef struct	s_flag
 {
@@ -30,3 +33,7 @@ int		ft_get_width(char *str, va_list *ap);
 int		ft_get_precision(char *str, va_list *ap);
 char	ft_get_length(char *str);
 char	ft_get_specifier(char *str);
+
+t_flag	*ft_parse_flags(const char *str, va_list *ap);
+
+#endif
