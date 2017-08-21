46e8bb04a0fdbb8f54c3b198a52d0d13b6c9541e
Youva GAUDE <ygaude@student.42.fr>
2017-08-21 16:02:53 +0200

    oops add ft_tools.c

---
+++ ft_tools.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tools.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygaude <ygaude@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/21 12:09:26 by ygaude            #+#    #+#             */
+/*   Updated: 2017/08/21 12:10:11 by ygaude           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdint.h>
+#include "libft/libft.h"
+
+char	*ft_addchar(char c, char **str)
+{
+	char	*buf;
+
+	c = (c < 10) ? c + '0' : c - 10 + 'a';
+	buf = ft_strnew(1);
+	buf[0] = c;
+	buf = ft_strappend(&buf, str, 'B');
+	return (buf);
+}
+
+char	*ft_uimaxtoa(uintmax_t n, unsigned int base)
+{
+	char	*str;
+
+	str = NULL;
+	if (!n)
+		str = ft_addchar('0', &str);
+	else
+	{
+		while (n)
+		{
+			str = ft_addchar((char)(n % base), &str);
+			n /= base;
+		}
+	}
+	return (str);
+}
+
+char	*ft_imaxtoa(intmax_t n, unsigned int base)
+{
+	char	*str;
+	char	*tmp;
+
+	tmp = "-";
+	str = ft_uimaxtoa((uintmax_t)((n < 0) ? -n : n), base);
+	return ((n < 0) ? ft_strappend(&tmp, &str, 'S') : str);
+}
