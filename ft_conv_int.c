c305c4c65b18e9ac3e8ef7fc63b944da0837d17b
Youva GAUDE <ygaude@student.42.fr>
2017-08-27 14:42:55 +0200

    add flags handling (WIP)

--- ft_conv_int.c
+++ ft_conv_int.c
@@ -6,7 +6,7 @@
 /*   By: ygaude <ygaude@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/26 18:28:01 by ygaude            #+#    #+#             */
-/*   Updated: 2017/08/27 10:24:16 by ygaude           ###   ########.fr       */
+/*   Updated: 2017/08/27 14:42:19 by ygaude           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,17 +36,58 @@ char	*ft_conv_int_prec(char *str, int prec)
 	return (str);
 }
 
-char	*ft_conv_int_width(char *str, t_flag flag)
+char	ft_conv_int_iszero(t_flag flag)
 {
-	size_t	len;
+	int		attributes;
 	char	c;
 
-	c = (flag.attributes & ZERO && flag.attributes & MINUS) ? '0' : ' ';
+	attributes = flag.attributes;
+	c = ' ';
+	if (attributes & ZERO && !(attributes & MINUS) && flag.precision == -1)
+		c = '0';
+	return (c);
+}
+
+char	*ft_conv_int_width(str, flag, c)
+{
+	char	*tmp;
+	size_t	len;
+
 	len = ft_strlen(str);
-	while (len++ < flag.width)
+	if (len < flag.width)
 	{
-		str = ft_addchar(
+		tmp = ft_strnew(flag.width - len);
+		ft_memset(tmp, c, flag.width - len);
+		if (flag.attributes & MINUS)
+			str = ft_strappend(&str, &tmp, 'B');
+		else
+			str = ft_strappend(&tmp, &str, 'B');
 	}
+	return (str);
+}
+
+char	*ft_conv_int_flag(char *str, t_flag flag)
+{
+	size_t	len;
+	char	*tmp;
+	char	c;
+
+	c = ft_conv_int(flag);
+	tmp = NULL;
+	if (c == '0')
+		str = ft_conv_int_width(str, flag, c);
+	if (flag.attribute & SHARP && ft_strchr("oxX", flag.specifier))
+		tmp = (flag.specifier == o) ? "0" : "0x";
+	else if (ft_strchr("di", flag.specifier))
+	{
+		if (flag.attribute & PLUS)
+			tmp = (*(intmax_t *)flag.value >= 0) ? "+" : "";
+		else if (flag.attribute & SPACE)
+			tmp = (*(intmax_t *)flag.value >= 0) ? " " : "";
+	}
+	str = ft_strappend(&tmp, &str, 'S');
+	if (c == ' ')
+		str = ft_conv_int_width(str, flag, c);
 	return (str);
 }
 
@@ -62,6 +103,6 @@ char	*ft_conv_int(t_flag flag)
 	else
 		str = ft_uimaxtoa(*(intmax_t *)(flag.value), base);
 	str = ft_conv_int_prec(str, flag.precision);
-	str = ft_conv_int_width(str, flag);
+	str = ft_conv_int_flag(str, flag);
 	return (str);
 }
