52592098434d1580c20176f4141667e00e77bfbe
Youva GAUDE <ygaude@student.42.fr>
2017-08-31 18:04:47 +0200

    Make a very small change on proto.sh

--- libft/proto.sh
+++ libft/proto.sh
@@ -1,5 +1,4 @@
 head -n 24 libft.h > temp.h
 cat *.c | grep "ft_" | grep "(" | grep -v ";" | grep -v "static" | grep -v "if" | grep -v "while" | sed 's/$/;/' >> temp.h
 echo "\n#endif" >> temp.h
-cat temp.h > libft.h
-rm temp.h
+mv temp.h libft.h
