4e254ce4b4e32bc15c20bf9605a876fce8b37c08
Youva GAUDE <ygaude@student.42.fr>
2017-09-04 05:34:14 +0200

    Correct ft_memjoin.c

--- libft/ft_memjoin.c
+++ libft/ft_memjoin.c
@@ -6,7 +6,7 @@
 /*   By: ygaude <ygaude@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/02 16:56:20 by ygaude            #+#    #+#             */
-/*   Updated: 2017/09/02 18:48:32 by ygaude           ###   ########.fr       */
+/*   Updated: 2017/09/04 04:57:09 by ygaude           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char				*ft_memjoin(void *p1, size_t len1, void *p2, size_t len2)
 {
 	char	*res;
 
-	if (!p1 || !p2 || len1 + len2 || !(res = (char *)ft_memalloc(len1 + len2)))
+	if (!p1 || !p2 || !(len1 + len2) || !(res = (char *)ft_memalloc(len1 + len2)))
 		return (NULL);
 	ft_memcpy(res, p1, len1);
 	ft_memcpy(res + len1, p2, len2);
