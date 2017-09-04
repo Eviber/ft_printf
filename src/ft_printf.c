e1eb9489a86023183278a77f8943e3d9db45d9c4
Youva GAUDE <ygaude@student.42.fr>
2017-09-04 05:35:29 +0200

    WIP fuck that percent character

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,14 +6,15 @@
 /*   By: ygaude <ygaude@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/31 18:09:19 by ygaude            #+#    #+#             */
-/*   Updated: 2017/09/03 23:25:23 by ygaude           ###   ########.fr       */
+/*   Updated: 2017/09/04 05:33:33 by ygaude           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <unistd.h>
 #include "../libft/libft.h"
 
-#define SPECIFIER "sSpdioOuUxXblablabla"
+#define SPECIFIER "sSpdDioOuUxXcC"
 
 typedef struct		s_str
 {
@@ -29,26 +30,37 @@ typedef struct		s_data
 	int				end;
 }					t_data;
 
-t_str	ft_flagappend(t_str *s1, t_str *s2, char c)
+t_str	ft_flagappend(t_str s1, t_str s2, char c)
 {
 	t_str	res;
 
-	if (s1 && s1->str && s2 && s2->str)
-		res.str = ft_memjoin(s1->str, s1->len, s2->str, s2->len);
-	else if (s1 && s1->str && *len)
-		res.str = ft_memdup(s1->str, s1->len);
-	else if (data && s2->str && s2->len)
-		res.str = ft_memdup(s2->str, s2->len);
+	res.len = s1.len + s2.len;
+	if (s1.str && s2.str && s1.len + s2.len)
+		res.str = ft_memjoin(s1.str, s1.len, s2.str, s2.len);
+	else if (s1.str && s1.len)
+		res.str = ft_memdup(s1.str, s1.len);
+	else if (s2.str && s2.len)
+		res.str = ft_memdup(s2.str, s2.len);
 	else
 		res.str = ft_memalloc(1);
 	if (c == 'F' || c == 'B')
-		ft_memdel((void **)(s1->str));
+		ft_memdel((void **)&(s1.str));
 	if (c == 'S' || c == 'B')
-		ft_memdel((void **)(s2->str));
+		ft_memdel((void **)&(s2.str));
 	return (res);
 }
 
-t_str	ft_loop(char *fmt, va_list ap)
+t_str	ft_domagic(t_str chunk, va_list ap)
+{
+	t_str	res;
+
+	(void)ap;
+	res.str = ft_memdup(chunk.str, chunk.len);
+	res.len = chunk.len;
+	return (res);
+}
+
+t_str	ft_loop(t_str fmt, va_list ap)
 {
 	t_data	data;
 	t_str	res;
@@ -57,21 +69,29 @@ t_str	ft_loop(char *fmt, va_list ap)
 	char	c;
 
 	ft_memset(&data, 0, sizeof(data));
+	ft_memset(&res, 0, sizeof(res));
 	fmt.len = (size_t)-1;
 	inflag = 0;
-	chunk.str = fmt;
-	while ((c = fmt[++fmt.len]))
+	chunk.str = fmt.str;
+	chunk.len = 0;
+	while ((c = fmt.str[++fmt.len]))
 	{
+		chunk.len++;
 		if (inflag && ft_strchr(SPECIFIER, c))
-			ft_flagappend(ft_domagic(fmt));
-		else if (c == '%')
-			ft_flagappend(&res, &len, &data, 'F');
-		if ((c == '%') || (inflag && ft_strchr(SPECIFIER, c)))
 		{
-			chunk.str = fmt + fmt_len;
-			inflag = !inflag;
+			res = ft_flagappend(res, ft_domagic(chunk, ap), 'B');
+			chunk.str = fmt.str + fmt.len;
+			inflag = 0;
+		}
+		else if (c == '%')
+		{
+			res = ft_flagappend(res, chunk, 'F');
+			chunk.str = fmt.str + fmt.len + 1;
+			inflag = 1;
 		}
 	}
+	if (chunk.str == fmt.str || inflag)
+		res = ft_flagappend(res, chunk, 'F');
 	return (res);
 }
 
@@ -81,16 +101,17 @@ int		ft_printf(const char *format, ...)
 	t_str	fmt;
 	t_str	res;
 
-	va_start(ap, format);
 	fmt.str = (char *)format;
+	va_start(ap, format);
 	res = ft_loop(fmt, ap);
-	if (res.len >= 0)
+	va_end(ap);
+	if (res.str)
 		write(1, res.str, res.len);
 	return (res.len);
 }
 
 int		main(void)
 {
-	ft_printf("Fils de pute\n");
+	ft_printf("test %%%%%%%% tt");
 	return (0);
 }
